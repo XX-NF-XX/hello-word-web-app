55cddd3b798d2eeae8e64f36c225a2d719ccd595
Night Flash <nightflash.box@gmail.com>
2018-06-01 01:04:04 +0300

    Add back-end script
    Add express, sqlite3 and body-parser modules.
    
    Add GET and POST methods required by comments section.

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('db/comments.db');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (request, response) {
+	response.status(200).redirect('hello.html');
+});
+
+app.get('/comments', function (request, response) {
+	console.log("GET request received at /comments");
+	db.all('SELECT * FROM comments', function (err, rows) {
+		if (err) {
+			console.log('Error: ' + err);
+		} else {
+			response.send(rows);
+		}
+	});
+});
+
+app.post('/comments', function (request, response) {
+	console.log("POST request received at /comments");
+	db.run('INSERT INTO comments VALUES (?, ?)', [request.body.userName, request.body.userComment], function (err) {
+		if (err) {
+			console.log("Error: " + err);
+		} else {
+			response.status(200).redirect('hello.html');
+		}
+	});
+});
+
+app.listen(3000, function () {
+	console.log("Server is running on port 3000");
+});
